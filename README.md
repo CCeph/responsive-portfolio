04c52604ac09ca01f86e822fce22ec71dc11c9d9
CCeph <96626515+CCeph@users.noreply.github.com>
2023-12-24 18:53:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jest, Webpack, Eslint, and Prettier Template
+This template is meant to be forked to easily setup a project with jest, webpack, eslint and prettier.
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+console.log("Webpack, ESLint, and Prettier Template");
+
+export default function checkTestingWorks(a, b) {
+  return a + b;
+}
